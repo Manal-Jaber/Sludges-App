bc1e7bea2145d66a331bfbadc2385e6e720b65de
mohammad-agha <mohammad.agha.dev@gmail.com>
2021-12-27 19:36:51 +0200

    apply an example for both contour and surface diagram

--- src/renderer/components/ContourDiagram/index.tsx
+++ src/renderer/components/ContourDiagram/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+
 import './index.scss';
 
 interface ContourDiagram {}
 
 const ContourDiagram: React.FC<ContourDiagram> = () => {
-  return <div className="countor-diagram">Countor Diagram</div>;
+  return (
+    <div className="contour-diagram">
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 3, 1, 3],
+            z: [1, 2, 3],
+            type: 'contour',
+            mode: 'gauge+number+delta',
+            marker: { color: 'red' },
+          },
+        ]}
+        layout={{
+          title: 'A Fancy Plot',
+          autosize: false,
+          width: 320,
+          height: 240,
+          margin: {
+            l: 65,
+            r: 50,
+            b: 65,
+            t: 90,
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default ContourDiagram;
--- src/renderer/components/SurfacePlot/index.tsx
+++ src/renderer/components/SurfacePlot/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+
 import './index.scss';
 
 interface SurfacePlot {}
 
 const SurfacePlot: React.FC<SurfacePlot> = () => {
-  return <div className="surface-plot">Surface Plot</div>;
+  return (
+    <div className="surface-plot">
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 3, 1, 3],
+            z: [1, 2, 3],
+            type: 'surface',
+            mode: 'gauge+number+delta',
+            marker: { color: 'red' },
+          },
+        ]}
+        layout={{
+          title: 'A Fancy Plot',
+          autosize: false,
+          width: 320,
+          height: 240,
+          margin: {
+            l: 65,
+            r: 50,
+            b: 65,
+            t: 90,
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default SurfacePlot;
